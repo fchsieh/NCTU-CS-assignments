956de1fac133e3113f8c14b299ea1789abaeae08
fchsieh <fchsieh@linux4.cs.nctu.edu.tw>
2018-10-02 13:02:50 +0800

    Update compiler

--- Compiler_2018_Fall/Project1/lex.yy.c
+++ Compiler_2018_Fall/Project1/lex.yy.c
@@ -360,8 +360,8 @@ static void yy_fatal_error (yyconst char msg[]  );
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
 
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
+#define YY_NUM_RULES 1
+#define YY_END_OF_BUFFER 2
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -369,17 +369,17 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static yyconst flex_int16_t yy_accept[8] =
+static yyconst flex_int16_t yy_accept[6] =
     {   0,
-        0,    0,    5,    3,    2,    1,    0
+        0,    0,    2,    1,    0
     } ;
 
 static yyconst flex_int32_t yy_ec[256] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    3,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -406,29 +406,29 @@ static yyconst flex_int32_t yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst flex_int32_t yy_meta[4] =
+static yyconst flex_int32_t yy_meta[2] =
     {   0,
-        1,    1,    1
+        1
     } ;
 
-static yyconst flex_int16_t yy_base[8] =
+static yyconst flex_int16_t yy_base[7] =
     {   0,
-        0,    0,    4,    5,    5,    5,    5
+        0,    0,    2,    3,    3,    0
     } ;
 
-static yyconst flex_int16_t yy_def[8] =
+static yyconst flex_int16_t yy_def[7] =
     {   0,
-        7,    1,    7,    7,    7,    7,    0
+        6,    6,    5,    5,    0,    5
     } ;
 
-static yyconst flex_int16_t yy_nxt[9] =
+static yyconst flex_int16_t yy_nxt[5] =
     {   0,
-        4,    5,    6,    7,    3,    7,    7,    7
+        4,    5,    3,    5
     } ;
 
-static yyconst flex_int16_t yy_chk[9] =
+static yyconst flex_int16_t yy_chk[5] =
     {   0,
-        1,    1,    1,    3,    7,    7,    7,    7
+        6,    3,    5,    5
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -447,17 +447,8 @@ int yy_flex_debug = 0;
 char *yytext;
 #line 1 "lextemplate.l"
 #line 2 "lextemplate.l"
-#define LIST                { strncat(buf, yytext, sizeof(buf) - strlen(buf) - 1); }
-#define token(t)            { LIST; if (Opt_Token) printf("<%s>\n", #t); }
-#define tokenString(t, s)   { LIST; if (Opt_Token) printf("<%s:%s>\n", t, s); }
-#define MAX_LINE_LENGTH 257
-#define MAX_ID_LENGTH 257
-int Opt_Source = 1;
-int Opt_Token = 1;
-int Opt_Statistic = 1;
-int linenum = 1;
-char buf[MAX_LINE_LENGTH];
-#line 461 "lex.yy.c"
+
+#line 452 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -639,9 +630,10 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
     
-#line 14 "lextemplate.l"
+#line 5 "lextemplate.l"
 
-#line 645 "lex.yy.c"
+
+#line 637 "lex.yy.c"
 
 	if ( !(yy_init) )
 		{
@@ -694,13 +686,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 8 )
+				if ( yy_current_state >= 6 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 5 );
+		while ( yy_base[yy_current_state] != 3 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -726,37 +718,10 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 15 "lextemplate.l"
-{
-    tokenString("delim", "("); 
-}
-	YY_BREAK
-case 2:
-/* rule 2 can match eol */
-YY_RULE_SETUP
-#line 19 "lextemplate.l"
-{
-    LIST;
-    if (Opt_Source)
-        printf("%d:%s\n", linenum, buf);
-    ++linenum;
-    buf[0] = '\0';
-}
-	YY_BREAK
-case 3:
-YY_RULE_SETUP
-#line 27 "lextemplate.l"
-{ // Unrecognized character
-    fprintf(stderr, "Error at line %d: %s\n", linenum, yytext);
-    exit(1);
-}
-	YY_BREAK
-case 4:
-YY_RULE_SETUP
-#line 32 "lextemplate.l"
+#line 7 "lextemplate.l"
 ECHO;
 	YY_BREAK
-#line 760 "lex.yy.c"
+#line 725 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1048,7 +1013,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 8 )
+			if ( yy_current_state >= 6 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1076,11 +1041,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 8 )
+		if ( yy_current_state >= 6 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 7);
+	yy_is_jam = (yy_current_state == 5);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1750,31 +1715,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 32 "lextemplate.l"
+#line 7 "lextemplate.l"
 
 
 
-int main( int argc, char **argv )
-{
-    if (argc >= 2)
-    {
-        yyin = fopen( argv[1], "r" );
-        if ( NULL == yyin ) {
-            fprintf( stderr, "Cannot open: %s\n", argv[1] );
-            exit(-1);
-        }
-    }
-    else
-    {
-        yyin = stdin;
-    }
-
-    yylex();
-
-    if (Opt_Statistic)
-    {
-        // Print frequency table
-    }
-
-    exit(0);
-}
