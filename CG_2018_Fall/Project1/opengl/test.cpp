9b426e0fff10ea4e22cc72be005d555fdaae6fa5
unknown <asd2260123@gmail.com>
2018-10-10 21:50:51 +0800

    Update CG p1

---
+++ CG_2018_Fall/Project1/opengl/test.cpp
@@ -0,0 +1,40 @@
+/*
+#include "glut.h"
+
+void display();
+int width = 400, height = 400;
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitWindowSize(width, height);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutCreateWindow("WindowName");
+
+	glutDisplayFunc(display);
+	glutMainLoop();
+
+	return 0;
+}
+
+void display()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glViewport(0, 0, width, height);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45, width / (GLfloat)height, 0.1, 1000);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(0.0f, 0.0f, 10.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glutSolidTeapot(1);
+
+	glutSwapBuffers();
+}
+*/
