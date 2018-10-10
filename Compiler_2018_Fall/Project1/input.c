9bab92bec338f940eb5df7b65c6fc6a257171304
FCHsieh <asd2260123@gmail.com>
2018-10-10 17:26:07 +0800

    Finish project1

---
+++ Compiler_2018_Fall/Project1/input.c
@@ -0,0 +1,16 @@
+#pragma   statistic   on// turn off statistic
+#pragma  token   off/* turn off token*/
+#pragma  source off
+
+// print hello world
+void main()
+{
+    int a;
+    double b;
+    print "hello world";
+    a = 1+1;
+    b = 1.23;
+    if (a > 1){
+        b = b*1.23e-1;
+    }
+}
