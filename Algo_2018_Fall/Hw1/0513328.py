72df5aadd0bdd0b14966ab94718cfd0c6cef184c
FCHsieh <asd2260123@gmail.com>
2018-10-13 00:58:04 +0800

    Update Youngtable.py

--- Algo_2018_Fall/Hw1/0513328.py
+++ Algo_2018_Fall/Hw1/0513328.py
@@ -1,16 +1,17 @@
 
 # Global variables
 TableCount = 0
-
+result = ""
 
 def read_file():
     with open("input.txt", "r") as input:
         TableCount = input.readline()
         print(TableCount)
 
+
 def print_output():
-    with open("output.txt", "r") as output:
-        output.write(result)
+    # temp
+    print("hi")
 
 
 def main():
@@ -18,6 +19,7 @@ def main():
         Run main function, and print result to output.txt
     """
     read_file()
+
 
 if __name__ == "__main__":
     main()
