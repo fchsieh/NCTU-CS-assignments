8b550751826550637c80c47a2de3d33ea506e96a
FCHsieh <fchsieh@cs.nctu.edu.tw>
2019-03-03 23:07:21 +0800

    Done ML_1

--- ML_2019_Spring/Hw1/Hw1.py
+++ ML_2019_Spring/Hw1/Hw1.py
@@ -1,17 +1,22 @@
-import matplotlib.pyplot as plt
-import random
 import math
-from pandas import *  # for debug
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import MatOP as mat
+import PolyReg as poly
 
 
 def generateTest():
+    start = random.random() * -5
+    cases = random.randint(15, 30)
+    x = start
     with open("testfile.txt", "w") as file:
-        for _ in range(math.floor(random.randint(15, 30))):  # random testcases
-            seed = random.randint(-50, 50)
-            x = str(random.random() * seed)
-            y = str(random.random() * seed)
-            test = x + "," + y + "\n"
-            file.write(test)
+        for _ in range(cases):
+            x = x + random.random()
+            y = math.cos(x) + random.random() * random.random()  # shift
+            file.write(str(x) + "," + str(y) + "\n")
 
 
 def readInputFile():
@@ -24,163 +29,55 @@ def readInputFile():
     return matrix
 
 
-def transpose(mat):
-    return list(map(list, zip(*mat)))
+def printRes_LSE(coeff, error):
+    # LSE:
+    print("LSE:")
+    print("Fitting line: ", end="")
+    for j in range(len(coeff)):
+        print(coeff[j], end="")
+        if (j != len(coeff) - 1):
+            print("X^%d + " % (len(coeff) - 1 - j), end="")
+    print("\nTotal error: %f" % error)
 
 
-def matmult(mat1, mat2):
-    row = []
-    result = []
-    for i in range(len(mat1)):
-        row = []
-        for j in range(len(mat2[0])):
-            sums = 0
-            for k in range(len(mat2)):
-                sums += mat1[i][k] * mat2[k][j]
-            row.append(sums)
-        result.append(row)
-    return result
-
-
-def lambdaI(lambda_, len):
-    m = []
-    for i in range(len):
-        r = []
-        for j in range(len):
-            if i == j:
-                r.append(lambda_)
-            else:
-                r.append(0)
-        m.append(r)
-    return m
-
-
-def LUdecomp(mat):
-    l_temp = []
-    for j in range(len(mat)):
-        l_tmp = lambdaI(1, len(mat))  # init Ln
-        l_tmp_inv = lambdaI(1, len(mat))
-        for i in range(j + 1, len(mat)):
-            l_tmp[i][j] = -mat[i][j] / mat[j][j]
-            l_tmp_inv[i][j] = mat[i][j] / mat[j][j]
-
-        mat = matmult(l_tmp, mat)  # Ln * Ln-1 * ... L1 * A = U
-        l_temp.append(l_tmp_inv)  # L = L1^-1 * L2^-1 * ... * Ln^-1
-
-    l = lambdaI(1, len(mat))
-    for i in range(len(l_temp)):
-        l = matmult(l, l_temp[i])
-
-    # return l, u
-    return l, mat  # final mat = u
-
-
-def forward(L, c):  # Lz = c
-    z = []
-    for i in range(len(c)):
-        z.append(c[i])
-        for j in range(i):
-            z[i] = z[i] - L[i][j] * z[j]
-        z[i] /= L[i][i]
-    return z
-
-
-def backward(U, z):  # Ux = z
-    x = []
-    for i in range(len(z)):
-        x.append(0)
-    for i in range(len(z) - 1, -1, -1):
-        x[i] += z[i]
-        for j in range(len(z) - 1, i, -1):
-            x[i] = x[i] - U[i][j] * x[j]
-        x[i] /= U[i][i]
-    return x  # x is the column vector of the inverse matrix
-
-
-def inverse(mat):
-    res = []
-    l, u = LUdecomp(mat)  # get A = LU
-    dim = len(mat)
-    c = lambdaI(1, dim)  # identity matrix c
-    z = []
-    # Solve z matrix: Lz = c
-    for i in range(dim):
-        z.append(forward(l, transpose(c)[i]))
-    z = transpose(z)
-    # Solve x matrix: Ux = z
-    for i in range(dim):
-        res.append(backward(u, transpose(z)[i]))
-    res = transpose(res)
-    return res  # A^-1
-
-
-class PolyReg():
-    # private:
-    def __init__(self, dimension, lambda_, data):
-        self.data = data
-        self.dim = dimension - 1  # n = ?, minor bug here, hard to fix
-        self.lambda_ = lambda_  # lambda = ?
-        self.matA = []  # A
-        self.vecX = []  # x
-        self.vecB = []  # b
-        self.__buildMatA()  # build matrix A and b
-        self.__buildVecB()
-
-    def __buildMatA(self):
-        for datarow in self.data:
-            x = datarow[0]
-            n = self.dim
-            row = []
-            while n >= 0:
-                row.append(pow(x, n))
-                n -= 1
-            self.matA.append(row)
-
-    def __buildVecB(self):
-        for datarow in self.data:
-            self.vecB.append([datarow[1]])
-
-    # public:
-    def rLSE(self):
-        x = []
-        rATA = matmult(transpose(self.matA), self.matA)  # A^TA
-        for i in range(len(rATA)):
-            for j in range(len(rATA[0])):
-                rATA[i][j] += lambdaI(self.lambda_, len(
-                    self.matA[0]))[i][j]  # do regularization
-        rATA = inverse(rATA)  # (A^TA + lambdaI)^-1
-        x = matmult(rATA, transpose(self.matA))  # (A^TA + lambdaI)^-1A^Tb
-        x = matmult(x, self.vecB)
-        flat_x = [item for sublist in x for item in sublist
-                  ]  # reduce dim(list of list to single list)
-        return flat_x
-
-    def visualize(self):
-        x = []
-        y = []
-        # plot data points
-        for datarow in self.data:
-            x.append(datarow[0])
-            y.append(datarow[1])
-        plt.plot(x, y, "ro")
-        figname = "n%s_lambda%s" % (self.dim, self.lambda_)
-        plt.savefig(figname)
+def printRes_Newton(coeff, error):
+    # Newton's
+    print("\nNewton's method: ")
+    print("Fitting line: ", end="")
+    for j in range(len(coeff)):
+        print(coeff[j], end="")
+        if (j != len(coeff) - 1):
+            print("X^%d + " % (len(coeff) - 1 - j), end="")
+    print("\nTotal error: %f" % error)
 
 
 def main():
-    #generateTest()
+    generateTest()
     data = readInputFile()
-    for i in range(2, 4):
-        print("n =", i, ", lambda = 0")
-        n = PolyReg(i, 0, data)
-        coeff = n.rLSE()
-        print("LSE:")
-        print("Fitting Line: ", end="")
-        for j in range(len(coeff)):
-            print(coeff[j], end="")
-            if (j != len(coeff) - 1):
-                print("X^%d + " % (len(coeff) - 1 - j), end="")
-        print("")
+    lambda_ = 0
+    for i in range(1, 11):
+        print("\nn =", i, ", lambda = %d" % lambda_)
+        n = poly.PolyReg(i, lambda_, data)
+        # LSE
+        coeff, err = n.rLSE()
+        printRes_LSE(coeff, err)
+        # Newton
+        coeff, err = n.Newton()
+        printRes_Newton(coeff, err)
+        # Visualize
+        n.visualize()
+
+    lambda_ = 10000
+    print("\nn = 3 , lambda = %d" % lambda_)
+    n = poly.PolyReg(3, lambda_, data)
+    # LSE
+    coeff, err = n.rLSE()
+    printRes_LSE(coeff, err)
+    # Newton's
+    coeff, err = n.Newton()
+    printRes_Newton(coeff, err)
+    # Visualize
+    n.visualize()
 
 
 if __name__ == "__main__":
---
+++ ML_2019_Spring/Hw1/PolyReg.py
@@ -0,0 +1,129 @@
+import math
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import MatOP as mat
+
+
+class PolyReg():
+    # private:
+    def __init__(self, dimension, lambda_, data):
+        self.data = data
+        self.dim = dimension - 1  # n = ?, minor bug here, hard to fix
+        self.lambda_ = lambda_  # lambda = ?
+        self.matA = []  # A
+        self.vecX = []  # x
+        self.vecB = []  # b
+        self.__buildMatA()  # build matrix A and b
+        self.__buildVecB()
+        self.LSEcoeff = []
+        self.NewtonCoeff = []
+
+    def __buildMatA(self):
+        for datarow in self.data:
+            x = datarow[0]
+            n = self.dim
+            row = []
+            while n >= 0:
+                row.append(pow(x, n))
+                n -= 1
+            self.matA.append(row)
+
+    def __buildVecB(self):
+        for datarow in self.data:
+            self.vecB.append([datarow[1]])
+
+    # public:
+    def rLSE(self):
+        x = []
+        rATA = mat.matrixMult(mat.transpose(self.matA), self.matA)  # A^TA
+        for i in range(len(rATA)):
+            for j in range(len(rATA[0])):
+                rATA[i][j] += mat.lambdaI(self.lambda_, len(
+                    self.matA[0]))[i][j]  # do regularization
+        rATA = mat.inverse(rATA)  # (A^TA + lambdaI)^-1
+        x = mat.matrixMult(rATA,
+                           mat.transpose(self.matA))  # (A^TA + lambdaI)^-1A^Tb
+        x = mat.matrixMult(x, self.vecB)  # our final result(coefficient)
+        flat_x = [item for sublist in x for item in sublist]  # flattern
+        self.LSEcoeff = flat_x
+
+        # Calculate error
+        error = 0
+        polyLSE = np.poly1d(self.LSEcoeff)
+        for datarow in self.data:
+            error += math.pow(datarow[1] - polyLSE(datarow[0]), 2)
+        return flat_x, error
+
+    def __gradient(self, A, b, x):
+        res = mat.transpose(self.matA)  # A^T
+        res = mat.matrixScalar(2, res)  # 2A^T
+        res = mat.matrixMult(res, self.matA)  # 2A^TA
+        res = mat.matrixMult(res, x)  # 2A^TAx
+        tmp = mat.matrixScalar(2, mat.transpose(self.matA))  # 2A^T
+        tmp = mat.matrixMult(tmp, b)  # 2A^Tb
+        res = mat.matrixSub(res, tmp)  # 2A^TAx - 2A^Tb
+        return res
+
+    def Newton(self, iters=100):
+        # Build Hessian^-1 = (2A^TA)^-1
+        AT = mat.transpose(self.matA)
+        Hessian = mat.matrixScalar(2, AT)
+        Hessian = mat.matrixMult(Hessian, self.matA)
+        Hessian = mat.inverse(Hessian)
+        # init point (x0)
+        x = []
+        for _ in range(self.dim + 1):
+            x.append([0])
+
+        it = 0
+        while it <= iters:
+            x = mat.matrixSub(
+                x,
+                mat.matrixMult(Hessian, self.__gradient(
+                    self.matA, self.vecB, x)))
+            it += 1
+
+        flat_x = [item for sublist in x for item in sublist]  # flattern
+        self.NewtonCoeff = flat_x
+
+        # Calculate error
+        error = 0
+        polyNewton = np.poly1d(self.NewtonCoeff)
+        for datarow in self.data:
+            error += math.pow(datarow[1] - polyNewton(datarow[0]), 2)
+
+        return flat_x, error
+
+    def visualize(self):
+        x = []
+        y = []
+        # plot data points
+        for datarow in self.data:
+            x.append(datarow[0])
+            y.append(datarow[1])
+        plt.style.use("ggplot")
+        fig, axs = plt.subplots(2, 1, constrained_layout=True)
+        fig.suptitle("n = %d, lambda = %d" % (self.dim + 1, self.lambda_))
+
+        # Plot LSE:
+        axs[0].set_title("LSE")
+        axs[0].set_xlabel("X")
+        axs[0].set_ylabel("y")
+        axs[0].plot(x, y, "bo")
+        polyLSE = np.poly1d(self.LSEcoeff)
+        polyLSE_y = polyLSE(x)
+        axs[0].plot(x, polyLSE_y)
+        # Plot Newton's
+        axs[1].set_title("Newton's method")
+        axs[1].set_xlabel("X")
+        axs[1].set_ylabel("y")
+        axs[1].plot(x, y, "bo")
+        polyNewton = np.poly1d(self.NewtonCoeff)
+        polyNewton_y = polyNewton(x)
+        axs[1].plot(x, polyNewton_y)
+
+        figname = "n%s_lambda%s" % (self.dim + 1, self.lambda_)
+        fig.savefig(figname)
+        plt.close()
